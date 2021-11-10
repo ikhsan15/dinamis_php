854fe9be1b888c9203d04c8e1328db5852f30d2f
ikhsan15 <ikhsan15nur@gmail.com>
2021-11-10 11:52:29 +0700

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title></title>
+		<script
+			src="https://code.jquery.com/jquery-3.3.1.js"
+			integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+			crossorigin="anonymous"
+		></script>
+	</head>
+	<body>
+		<?php include("header.html"); ?>
+		<div id="header"></div>
+		<h1>from index page</h1>
+		<!--Remaining section-->
+		<!-- <div id="footer"></div> -->
+	</body>
+</html>
